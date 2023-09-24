43571e6d6bf6e340708c25f469a13965a39c9231
Kevinoscal <145907554+Kevinoscal@users.noreply.github.com>
2023-09-24 05:58:08 -0600

    Add files via upload

---
+++ Ejercicio2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int cantidad;
+	cout<<"Ingrese la cantidad de elementos";
+	cin>>cantidad;
+		int suma=0;
+		int promedio;
+	int arreglo [arreglo];
+		for(int i=0; i<cantidad; i++){
+			suma=suma+arreglo[i];
+		}
+		promedio=suma/cantidad		
+		cout<<promedio;			
+	return 0;		
+}
+
+	
+
+
